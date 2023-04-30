6fd7768c6c77001502706f34b0b1f2f8dfdd6510
Dmitry <dimaremenec@gmail.com>
2023-04-30 19:50:59 +0300

    slash was added

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -4,7 +4,7 @@ from .views import PostsList, PostDetail, PostSearch, upgrade_me, subscribe_to_c
 
 urlpatterns = [
     path('', PostsList.as_view(), name='post_list'),
-    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
     path('search/', PostSearch.as_view(), name='post_search'),
     path('upgrade/', upgrade_me, name='upgrade'),
     path('category/subscribe/', subscribe_to_categories, name='subscribe_to_categories'),
