f3e9a318ec9a085af57ab31c5a68c485b89fbf57
Dmitry <dimaremenec@gmail.com>
2023-04-30 21:27:17 +0300

    articles/ path was added

--- NewsPaper/NewsPaper/urls.py
+++ NewsPaper/NewsPaper/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('pages/', include('django.contrib.flatpages.urls')),
     path('', lambda r: redirect('news/')),
     path('news/', include('news.urls')),
+    path('articles/', include('news.urls')),
     path('news/create/', NewsCreate.as_view(), name='news_create'),
     path('news/<int:pk>/edit/', NewsUpdate.as_view(), name='news_update'),
     path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
